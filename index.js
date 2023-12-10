90a507df80f0b1849ac731fd89bb7cbffadca7ba
numair2000 <numairshaikh45@gmail.com>
2023-12-10 17:14:19 +0530

    modified

--- index.js
+++ index.js
@@ -81,6 +81,45 @@ app.get('/registeredCount', async (req, res) => {
   }
 });
 
+mongoose.connect('mongodb://localhost:27017/martialarts', { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true 
+});
+// const db = mongoose.connection;
+
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// db.once('open', () => {
+//   console.log('Connected to MongoDB');
+// });
+
+// Create a schema for suggestions
+const suggestionSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  suggestion: String,
+  grievance: String,
+});
+
+const Suggestion = mongoose.model('Suggestion', suggestionSchema);
+
+// Endpoint for handling suggestion form submissions
+app.post('/submitSuggestion', async (req, res) => {
+  const { name, email, suggestion, grievance } = req.body;
+
+  try {
+    // Save the suggestion to the database
+    const newSuggestion = new Suggestion({ name, email, suggestion, grievance });
+    await newSuggestion.save();
+
+    // Respond with a success message
+    res.json({ message: 'Suggestion submitted successfully', success: true });
+  } catch (error) {
+    console.error('Error saving suggestion:', error);
+    res.status(500).json({ message: 'Internal server error', success: false });
+  }
+});
+
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
